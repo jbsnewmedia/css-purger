33b953e0c48a44477a369c2a8c37b440ada2b5e1
jschwind <info@juergen-schwind.de>
2025-12-08 17:34:08 +0100

    Add root and theme-based selectors to Bootstrap vendor class

--- src/Vendors/Bootstrap.php
+++ src/Vendors/Bootstrap.php
@@ -12,6 +12,12 @@ class Bootstrap extends CssPurger
         $this->cssBlockPrefix = substr($this->content, 0, strpos($this->content, ':root'));
         $this->content = str_replace("*/\n:root,", "*/\n}\n:root,", $this->content);
 
+        $this->addSelectors([
+            ':root',
+            '[data-bs-theme=light]',
+            '[data-bs-theme=dark]',
+        ]);
+
         return $this;
     }
 
